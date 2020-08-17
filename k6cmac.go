67a1d73f1e2272d795e8a2ce088d9e097d168046
Marduk Bolaños Puchet <mardukb@imbus.de>
2020-08-17 17:25:27 +0200

    Generate CMAC

---
+++ k6cmac.go
@@ -0,0 +1,21 @@
+package k6cmac
+
+import (
+	"context"
+	"crypto/aes"
+	"encoding/base64"
+	"github.com/aead/cmac"
+)
+
+type K6cmac struct{}
+
+func New() *K6cmac {
+	return &K6cmac{}
+}
+
+func (c *K6cmac) generateCmac(ctx context.Context, keyb64 string, data string) string {
+	key, _ := base64.StdEncoding.DecodeString(keyb64)
+	aesCipher, _ := aes.NewCipher(key)
+	signature, _ := cmac.Sum([]byte(data), aesCipher, 8)
+	return base64.StdEncoding.EncodeToString(signature)
+}
