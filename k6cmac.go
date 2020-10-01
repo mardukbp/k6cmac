0dfe46d5cd486ba24cf799ef37f78edc3fe5c506
Marduk Bolaños Puchet <mardukb@imbus.de>
2020-10-01 14:28:47 +0200

    Changed GenerateCmac to Cmac

--- k6cmac.go
+++ k6cmac.go
@@ -14,9 +14,13 @@ func New() *K6cmac {
 }
 
 // Method names must begin with a capital letter
-func (c *K6cmac) Generatecmac(ctx context.Context, keyb64 string, data string) string {
+func (c *K6cmac) Cmac(ctx context.Context, 
+                              keyb64 string, 
+                              data string) string {
+
 	key, _ := base64.StdEncoding.DecodeString(keyb64)
 	aesCipher, _ := aes.NewCipher(key)
-	signature, _ := cmac.Sum([]byte(data), aesCipher, 8)
+    blockSize := 8
+	signature, _ := cmac.Sum([]byte(data), aesCipher, blockSize)
 	return base64.StdEncoding.EncodeToString(signature)
 }
